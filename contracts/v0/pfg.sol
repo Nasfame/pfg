6608af436abbf09670569e7dbb0282547b114f14
Nasfame <laciferin@gmail.com>
2024-01-25 12:46:35 +0530

    unlock:

--- contracts/v0/pfg.sol
+++ contracts/v0/pfg.sol
@@ -4,21 +4,24 @@ pragma solidity ^0.8.20;
 import "hardhat/console.sol";
 
 contract PfgV0 {
+    uint public deltaUnlockTime = 1209600; //2weeks
     uint public unlockTime;
     address payable public owner;
+    uint public initialDeposit;
 
+    event Deposit(uint amount, uint when);
     event Withdrawal(uint amount, uint when);
 
     constructor() payable {
-        // Set the unlock time to 2 weeks (1209600 seconds) from the current block timestamp
-        unlockTime = block.timestamp + 1209600;
+        unlockTime = block.timestamp + deltaUnlockTime ;
 
         owner = payable(msg.sender);
+        initialDeposit = msg.value;
+
+        emit Deposit(initialDeposit, block.timestamp);
     }
 
     function withdraw() public {
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
         require(block.timestamp >= unlockTime, "You can't withdraw yet");
         require(msg.sender == owner, "You aren't the owner");
 
