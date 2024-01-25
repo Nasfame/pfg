dd0701c1dc498dc21247816d611608c52a488730
Nasfame <laciferin@gmail.com>
2024-01-25 23:20:12 +0530

    pfg: fix: proposalValue modifier

--- contracts/v0/pfg.sol
+++ contracts/v0/pfg.sol
@@ -49,6 +49,7 @@ contract PfgV0 {
     }
 
     modifier proposalValueCheck() {
+        proposalValue = msg.value;
         require(proposalValue!=0, "Proposal Value needs to be greator than 0");
         _;
     }
