2484727b9b9499b9b5e63e488dd8fd82770743cd
Hiro <laciferin@gmail.com>
2024-01-26 17:17:51 +0530

    deploy: final fixes hardhat deploy (#7)
    * deploy: final fixes
    
    * style: pretty code
    
    by ci
    
    ---------
    
    Co-authored-by: Nasfame <Nasfame@users.noreply.github.com>

--- scripts/deployPFGV0.ts
+++ scripts/deployPFGV0.ts
@@ -16,10 +16,9 @@ const deployPFG: DeployFunction = async function deployPFG({
   const GRANTOR_ADDRESS = ACCOUNT_ADDRESSES["GRANTOR"];
   const GRANTEE_ADDRESS = ACCOUNT_ADDRESSES["GRANTEE"];
   const DELTA_UNLOCK_TIME = 2 * 7 * 24 * 60 * 60; // 2 weeks in seconds
-  const PROPOSAL_VALUE = ethers.parseEther("1"); // Change to the desired value in Ether
+  const PROPOSAL_VALUE = ethers.parseEther(process.env.PROPOSAL_VALUE || "0.2"); // Change to the desired value in Ether
 
-
-  console.log("Deploy:Start")
+  console.log("Deploy:Start");
   // Deploy the contract
   // FIXME: fixme use hardhat-deploy (its not working)
   // const PfgV0 = await deploy("PfgV0", {
@@ -28,15 +27,17 @@ const deployPFG: DeployFunction = async function deployPFG({
   //   log: true,
   // });
 
-  const PfgV0 = await ethers.deployContract("PfgV0", [],{ //deploying to QB (as its 1st address in private keys passed in hardhat config) 
+  const PfgV0 = await ethers.deployContract("PfgV0", [], {
+    //deploying to QB (as its 1st address in private keys passed in hardhat config)
     value: PROPOSAL_VALUE,
   });
 
-  const PFGContractAddr = PfgV0.target //PFGV0.address 
+  const PFGContractAddr = PfgV0.target; //PFGV0.address
 
   console.log("PfgV0 deployed to:", PFGContractAddr);
-  console.log("PfgV0", PfgV0)
+  console.log("PfgV0", PfgV0);
 
+  //TODO: migrate if necessary some of the code to inialize function
 
   // Get the deployed contract instance
   // const pfgInstance = await ethers.getContractAt("PfgV0", PFGContractAddr);
