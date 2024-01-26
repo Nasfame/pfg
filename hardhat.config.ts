5bc4ae5c838ffac849ff8247eb374d91dbf06ad5
Nasfame <laciferin@gmail.com>
2024-01-26 15:07:06 +0530

    add; prettier

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,11 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
 
-import "hardhat-deploy";
+import "hardhat-deploy"
 
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
 const config: HardhatUserConfig = {
   defaultNetwork: "local",
@@ -14,13 +14,16 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [process.env.PRIVATE_KEY ?? ""],
+      tags: ["mainnet"],
     },
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [process.env.PRIVATE_KEY ?? ""],
+      tags: ["testnet"],
     },
     local: {
       url: "http://127.0.0.1:8545",
+      tags: ["local"],
     },
     lilypad: {
       url: "http://testnet.lilypad.tech:8545",
@@ -29,6 +32,7 @@ const config: HardhatUserConfig = {
         process.env.GRANTOR_PRIVATE_KEY,
         process.env.GRANTEE_PRIVATE_KEY,
       ],
+      tags: ["testnet", "lilypad"],
     },
   },
   solidity: {
@@ -49,6 +53,15 @@ const config: HardhatUserConfig = {
   mocha: {
     timeout: 40000,
   },
-};
 
-export default config;
+  namedAccounts: {
+    QB: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+      4: "0xA296a3d5F026953e17F472B497eC29a5631FB51B", // but for rinkeby it will be a specific address
+      goerli: "0x84b9514E013710b9dD0811c9Fe46b837a4A0d8E0", //it can also specify a specific netwotk name (specified in hardhat.config.js)
+    },
+  },
+}
+
+export default config
