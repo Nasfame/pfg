90881ab4ce16949a17f8fed57f0130e202a7ab44
Nasfame <laciferin@gmail.com>
2024-01-26 16:25:01 +0530

    nits:

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -40,7 +40,7 @@ const config: HardhatUserConfig = {
         {
           privateKey:
             process.env.PRIVATE_KEY ||
-            "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+            "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", //hardhat 01
           balance: `${1000000000000000000000000n}`,
         },
       ],
@@ -50,7 +50,7 @@ const config: HardhatUserConfig = {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
   solidity: {
-    version: "0.8.20",
+    version: "0.8.21",
     settings: {
       optimizer: {
         enabled: true,
